3115a1f666c1e3d85b7843e77b4e79a5931d7c4f
sergionarciso717@gmail.com <>
2022-04-28 18:43:02 -0500

    PRUEBA CES 1

---
+++ src/pruebaces1/PruebaCES1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebaces1;
+
+/**
+ *
+ * @author Narciso
+ */
+public class PruebaCES1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo PRUEBA  GIT HUB NARCISO LOPEZ 2842022");
+    }
+    
+}
